bcc26df986d1f92ac606449afabfeb902250093f
Hemant-Dua <135449755+Hemant-Dua@users.noreply.github.com>
2023-12-11 10:36:19 +0530

    Create hangman.py

---
+++ Hangman/hangman.py
@@ -0,0 +1,51 @@
+import random
+
+def choose_word():
+    words = ["programming", "hangman", "python", "challenge"]
+    return random.choice(words)
+
+def display_word(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+    return display
+
+def get_guess():
+    return input("Guess a letter: ").lower()
+
+def check_guess(guess, word):
+    return guess in word
+
+def update_guessed_letters(guess, guessed_letters):
+    guessed_letters.append(guess)
+
+def main():
+    word = choose_word()
+    guessed_letters = []
+    max_attempts = 6
+
+    while max_attempts > 0:
+        current_display = display_word(word, guessed_letters)
+        print(current_display)
+
+        guess = get_guess()
+
+        if check_guess(guess, word):
+            print("Correct!")
+        else:
+            print("Incorrect!")
+            max_attempts -= 1
+
+        update_guessed_letters(guess, guessed_letters)
+
+        if set(word).issubset(guessed_letters):
+            print("Congratulations! You guessed the word:", word)
+            break
+
+    if max_attempts == 0:
+        print("Sorry, you ran out of attempts. The word was:", word)
+
+main()
