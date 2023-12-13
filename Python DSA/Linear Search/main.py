44f62f795f9322f98e63f49b8cd4be1764a1cbfb
Hemant-Dua <135449755+Hemant-Dua@users.noreply.github.com>
2023-12-14 00:22:07 +0530

    Linear Search

---
+++ Python DSA/Linear Search/main.py
@@ -0,0 +1,25 @@
+def search(arr,size,x):
+    for i in range(0,size):
+        if arr[i] == x:
+            return i
+    return -1
+
+
+if __name__ == "__main__":
+    arr_size=int(input("Enter the number of elements in array: "))
+
+    arr=[]
+    print("Enter the elements of the array: ")
+    for i in range(0,arr_size):
+        x=int(input())
+        arr.append(x)
+
+    target=int(input("Enter the target: "))
+
+    result = search(arr, arr_size, target)
+    
+    if result == -1:
+        print("The required number is not in the array.")
+
+    else:
+        print(f"The Target is present at the index: {result}")
