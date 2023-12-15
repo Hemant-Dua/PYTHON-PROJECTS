a0f52f7db0cc6d0d945b4f065a8ad411a1d47260
Hemant-Dua <135449755+Hemant-Dua@users.noreply.github.com>
2023-12-15 21:08:51 +0530

    Update qr.py

--- QR Generator/qr.py
+++ QR Generator/qr.py
@@ -1,11 +1,5 @@
-# Importing library
 import qrcode
 
-# Data to be encoded
 data = input("Type the Message : ")
- 
-# Encoding data using make() function
 img = qrcode.make(data)
- 
-# Saving as an image file
 img.save('QR Generator//MyQRCode.png')
