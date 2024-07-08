38c5ed310bf2ab90e0a251cd6fba8da1d163f291
Hemant-Dua <135449755+Hemant-Dua@users.noreply.github.com>
2024-07-08 07:42:54 +0530

    Update main.py

--- Python DSA/Bubble Sort/main.py
+++ Python DSA/Bubble Sort/main.py
@@ -1,12 +1,11 @@
-def bubblesort(arr,size):
-    passes = 0
-    while (passes != size):
-        for i in range(0,size):
+def bubblesort(arr):
+    flag = True
+    while (flag == True):
+        flag = False
+        for i in range(len(arr)-1):
             if arr[i] > arr[i+1]:
-                temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
-        passes+=1
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                flag = True
     return arr
 
 if __name__ == "__main__":
@@ -14,10 +13,10 @@ if __name__ == "__main__":
 
     arr = []
     print("Enter the elements of the array: ")
-    for i in range(0,size):
+    for i in range(size):
         x = int(input())
         arr.append(x)
     
     print(f"The Given array is: {arr}")
-    arr = bubblesort(arr,size-1)
+    arr = bubblesort(arr)
     print(f"The Sorted array will be: {arr}")
